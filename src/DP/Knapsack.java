c04572d8138b2095528000c2c2d0e77d4d345f87
Kalpana <KALPANAKATHAIT11@GMAIL.COM>
2022-06-01 08:50:16 +0530

    all done now

--- src/DP/Knapsack.java
+++ src/DP/Knapsack.java
@@ -1,10 +1,12 @@
 package DP;
 
+import java.util.Scanner;
+
 public class Knapsack {
-//int[] weight;
+    //int[] weight;
 //int[] price;
 //int W;//capacity
-int[][]dp;
+    int[][]dp;
     Knapsack(int n,int W)
     {
         dp=new int[100][100];
@@ -21,26 +23,45 @@ int[][]dp;
     }
 
     public static void main(String[] args) {
-    Knapsack ob=new Knapsack(5,18);
-    int res=ob.O_1_KnapsaIter(new int[]{1,3,7,4,5},new int[]{1,4,7,5,7},18,5);
-        System.out.println(ob.O_1_KnapsackMem(new int[]{1,3,7,4,5},new int[]{1,4,7,5,7},18,5));
-        System.out.println(res);
-}
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array ");
+        int n=sc.nextInt();
+        int wt[]=new int[n];
+        int []profit=new int[n];
+        for(int i=0;i<n;i++){
+            wt[i]= sc.nextInt();
+            profit[i]=sc.nextInt();
+        }
+        System.out.println("Enter the capacity of knapsack");
+
+        int W=sc.nextInt();
 
 
-//Recursive only
+        Knapsack ob=new Knapsack(n,W);
+        System.out.println(ob.O_1_Knapsack(wt,profit,W,n));
+        System.out.println(ob.O_1_KnapsackMem(wt,profit,W,n));
+        System.out.println(ob.O_1_KnapsaIter(wt,profit,W,n));
+//
+//        2 10 3 5 5 15 7 7 1 6 4 18 1 3
+//         5 15 7 6 18 3
+//        7
+//        15
+    }
+
+
+    //Recursive only
     int O_1_Knapsack(int[]weight,int[] price,int W,int n){
         if(n==0||W==0)return 0;
 
         if(weight[n-1]>W)return O_1_Knapsack(weight,price,W,n-1);
 
-            else return Math.max(price[n-1]+O_1_Knapsack(weight,price,W-weight[n-1],n-1),
+        else return Math.max(price[n-1]+O_1_Knapsack(weight,price,W-weight[n-1],n-1),
                 O_1_Knapsack(weight,price,W,n-1));
 
     }
 
 
-//    Recursive + DP (taking a matrix to reduce extra recursive calls to happen)
+    //    Recursive + DP (taking a matrix to reduce extra recursive calls to happen)
     int O_1_KnapsackMem(int[]weight,int[] price,int W,int n){
         if(n==0||W==0)return 0;
 
@@ -53,26 +74,33 @@ int[][]dp;
     }
 
     int O_1_KnapsaIter(int[]weight,int[] price,int W,int n){
+        int[][]t=new int[n+1][W+1];
+
         for (int i = 1; i < n+1; i++) {
             for (int j = 1; j < W + 1; j++) {
-                if (i == 0 || j == 0) dp[i][j] = 0;
+                if (i == 0 || j == 0) t[i][j] = 0;
             }
+        }
+
+        for (int[]arr:t) {
+            for(int i:arr)
+                System.out.print(i);
+            System.out.println();
         }
         for (int i = 1; i < n+1; i++) {
             for (int j = 1; j < W+1; j++) {
 
                 if(weight[i-1]>j)
 
-                  dp[i][j]= dp[i-1][j];
+                    t[i][j]= t[i-1][j];
 
-                else dp[i][j]=Math.max(price[i-1]+dp[i-1][j-weight[i-1]],
-                        dp[i-1][j]);
+                else t[i][j]=Math.max(price[i-1]+t[i-1][j-weight[i-1]],
+                        t[i-1][j]);
             }
         }
-        return dp[n][W];
+        return t[n][W];
 
 
     }
 
 }
-
